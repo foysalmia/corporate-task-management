b852246a3cb6d830ca4ea6d3ad64a829036d5a74
Foysal Ahmed <mfoysal314@gmail.com>
2024-02-07 12:25:08 +0600

    admin panel creation done

--- asset_manage/admin.py
+++ asset_manage/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    ordering = ['name']
+    list_per_page = 10
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ['name','designation']
+    list_per_page = 15
+
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    list_per_page = 20
+
+@admin.register(DeviceLog)
+class DeviceLogAdmin(admin.ModelAdmin):
+    list_display = ['name', 'checkout_time','return_time']
+    ordering = ['checkout_time','return_time']
+    list_per_page = 20
+
+
--- asset_manage/models.py
+++ asset_manage/models.py
@@ -5,15 +5,28 @@ class Company(models.Model):
     name = models.CharField(max_length = 150)
     description = models.TextField(blank=True,null=True)
 
+    def __str__(self):
+        return self.name
+
 class Employee(models.Model):
     user = models.OneToOneField(User,on_delete = models.CASCADE)
     company = models.ForeignKey(Company,on_delete = models.CASCADE, related_name = 'employee')
     designation = models.CharField(max_length = 250)
 
+    def name(self):
+        return f"{self.user.first_name} {self.user.last_name}"
+
+    def __str__(self):
+        return str(f"{self.user.first_name} {self.user.last_name}")
+
+
 class Device(models.Model):
     name = models.CharField(max_length = 250)
     condition = models.CharField(max_length = 250)
     company = models.ForeignKey(Company,on_delete = models.CASCADE, related_name = 'device')
+
+    def __str__(self):
+        return self.name
 
 class DeviceLog(models.Model):
     device = models.ForeignKey(Device,on_delete = models.CASCADE , related_name = 'log')
@@ -21,5 +34,8 @@ class DeviceLog(models.Model):
     checkout_time = models.DateField(auto_now_add = True)
     return_time = models.DateField()
     checkout_condition = models.CharField(max_length= 255)
-    etuern_condition = models.CharField(max_length= 255)
+    return_condition = models.CharField(max_length= 255)
+
+    def name(self):
+        return self.device.name
 
