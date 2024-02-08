ad1050ce63c5327a2054cb8f3f694c772ff2dfcb
Foysal Ahmed <mfoysal314@gmail.com>
2024-02-08 16:15:33 +0600

    permissions added in api layers

--- asset_manage/views.py
+++ asset_manage/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAdminUser,IsAuthenticatedOrReadOnly,IsAuthenticated
 
 from .serializers import *
 from .models import *
@@ -6,9 +7,16 @@ from .models import *
 class CompanyViewSet(ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class EmployeeViewSet(ModelViewSet):
     queryset = Employee.objects.all()
+    permission_classes = [IsAdminUser]
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [IsAuthenticated()]
+        return [IsAdminUser()]
 
     def get_serializer_class(self):
         if self.request.method == 'GET':
@@ -17,6 +25,11 @@ class EmployeeViewSet(ModelViewSet):
 
 class DeviceViewSet(ModelViewSet):
     queryset = Device.objects.all()
+    
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [IsAuthenticatedOrReadOnly()]
+        return [IsAdminUser()]
 
     def get_serializer_class(self):
         if self.request.method == 'GET':
@@ -25,6 +38,11 @@ class DeviceViewSet(ModelViewSet):
 
 class DeviceLogViewSet(ModelViewSet):
     queryset = DeviceLog.objects.all()
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [IsAuthenticatedOrReadOnly()]
+        return [IsAdminUser()]
 
     def get_serializer_class(self):
         if self.request.method == 'GET':
